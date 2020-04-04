37a5fad438632c6ab4bad26e3d8b8825630dc659
Maikel Schuurmans <264235@student.fontys.nl>
2020-04-04 23:54:54 +0200

    Added state machine to Enemy

---
+++ PortalShuriken/Assets/Scripts/Enemy/Behaviours/ChaseBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseBehaviour : StateMachineBehaviour
+{
+    public float speed = 8f;
+
+    private Enemy enemy;
+    private NavMeshAgent navMeshAgent;
+    private FieldOfView fieldOfView;
+
+    private int currentPatrolIndex = 0;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        enemy = animator.GetComponent<Enemy>();
+        navMeshAgent = enemy.navMeshAgent;
+        fieldOfView = enemy.fieldOfView;
+
+        navMeshAgent.enabled = true;
+        navMeshAgent.isStopped = false;
+
+        navMeshAgent.speed = speed;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        ChasePlayer();
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    private void ChasePlayer()
+    {
+        if (navMeshAgent.enabled)
+        {
+            navMeshAgent.destination = enemy.lastPlayerPos;
+        }
+    }
+
+    private bool IsEnemyNearDestination()
+    {
+        if (navMeshAgent.pathPending) return false;
+        if (!(navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)) return false;
+        return !navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f;
+    }
+}
--- PortalShuriken/Assets/Scripts/Enemy/Enemy.cs
+++ PortalShuriken/Assets/Scripts/Enemy/Enemy.cs
@@ -1,19 +1,64 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public int health = 100;
+    public float awareness = 0f;
+    public float awarenessIncreaseSpeed = 30;
+    public float awarenessDecreaseSpeed = 20;
+    public Vector3 lastPlayerPos;
+
+    public FieldOfView fieldOfView;
+    public NavMeshAgent navMeshAgent;
+    public Rigidbody rigidbody;
+    public List<Transform> patrolPositions;
+
+    private Animator enemyAI;
+
+    public 
+
     // Start is called before the first frame update
     void Start()
     {
-
+        enemyAI = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < -20)
+        if (fieldOfView.visibleTargets.Count > 0)
+        {
+            awareness += awarenessIncreaseSpeed * Time.deltaTime;
+            lastPlayerPos = fieldOfView.visibleTargets[0].position;
+        }
+        else
+        {
+            awareness -= awarenessDecreaseSpeed * Time.deltaTime;
+        }
+
+        awareness = Mathf.Clamp(awareness, 0, 100);
+        enemyAI.SetFloat("awareness", awareness);
+
+        var color = Color.Lerp(Color.green, Color.red, awareness / 100);
+        color.a = .5f;
+        fieldOfView.viewMeshFilter.GetComponent<Renderer>().material.color = color;
+
+        if (transform.position.y < -100)
         {
             Destroy(transform.gameObject);
+        }
+    }
+
+    public void StunEnemy()
+    {
+        enemyAI.SetBool("stunned", true);
+        enemyAI.SetTrigger("stun");
+        if (navMeshAgent.enabled)
+        {
+            navMeshAgent.isStopped = true;
+            navMeshAgent.enabled = false;
         }
     }
 }
