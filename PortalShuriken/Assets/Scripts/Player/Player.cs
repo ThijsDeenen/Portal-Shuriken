f59c3c69f08fdb42d17b12d6efdc45f865a9deed
Thijs Deenen <thijsdeenen@kpnmail.nl>
2020-04-17 22:27:38 +0200

    Added Main Menu And Game Over

--- PortalShuriken/Assets/Scripts/Player/Player.cs
+++ PortalShuriken/Assets/Scripts/Player/Player.cs
@@ -5,27 +5,50 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private int health = 100;
     [HideInInspector] public UnityEvent healthUpdateEvent = new UnityEvent();
-
-    // Start is called before the first frame update
-    void Start()
+    public Death death;
+    private Transform[] objectsToDisable;
+    private Component[] behavioursToDisable;
+    private GameObject cam;
+    private void Start()
     {
+        objectsToDisable = gameObject.GetComponentsInChildren<Transform>();
 
+        cam = gameObject.transform.GetChild(0).gameObject;
+
+        behavioursToDisable = gameObject.GetComponents(typeof(Behaviour));
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void UpdateHealth(int change)
     {
         health += change;
         healthUpdateEvent.Invoke();
+        if (health <= 0)
+        {
+            Die();
+        }
     }
 
     public int GetHealth()
     {
         return health;
+    }
+
+    private void Die()
+    {
+        death.GameOverUI();
+        for (int i = 2; i < objectsToDisable.Length - 1; i++)
+        {
+            objectsToDisable[i].gameObject.SetActive(false);
+        }
+
+        cam.GetComponent<MouseLook>().enabled = false;
+
+        foreach (Behaviour item in behavioursToDisable)
+        {
+            if (item != GetComponent(typeof(Teleport)))
+            {
+                item.enabled = false;
+            }
+        }
+        gameObject.GetComponent<CharacterController>().enabled = false;
     }
 }
